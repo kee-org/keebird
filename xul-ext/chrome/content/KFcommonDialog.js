kee-org/keebird

commit 7b3959d1af8883d88d04dbdac5c7ac319ce7ca18

Convert Cu.import to ChromeUtils.import
Also comment out unused imports

xul-ext/chrome/content/KFcommonDialog.js
@@ -37,7 +37,7 @@ if (!Ci)
 if (!Cu)
     var Cu = Components.utils;
 
-Cu.import("resource://kfmod/KF.js");
+const { keefox_org } = ChromeUtils.import("resource://kfmod/KF.js");
 
 var keeFoxDialogManager = {
     scriptLoader : Components.classes["@mozilla.org/moz/jssubscript-loader;1"]

xul-ext/chrome/content/gdata-provider.js
@@ -33,7 +33,7 @@ if (!Ci)
 if (!Cu)
     var Cu = Components.utils;
 
-Cu.import("resource://kfmod/KF.js");
+const { keefox_org } = ChromeUtils.import("resource://kfmod/KF.js");
 
 var keeFoxGDataProviderHelper = {
     scriptLoader : Components.classes["@mozilla.org/moz/jssubscript-loader;1"]

xul-ext/chrome/content/PasswordSaver.js
@@ -28,9 +28,9 @@ keefox_win.PasswordSaver = function(doc, saveData, URLs, saveButtonCallback)
     this.URLs = URLs;
     this.saveButtonCallback = saveButtonCallback;
 
-    Cu.import("resource://kfmod/search.js", this);
+    const { Search } = ChromeUtils.import("resource://kfmod/search.js");
 
-    this.search = new this.Search(keefox_org, {
+    this.search = new Search(keefox_org, {
         version: 1,
         searchAllDatabases: true,
         maximumResults: 50

xul-ext/chrome/content/UninstallHelper.js
@@ -22,7 +22,7 @@
 */
 "use strict";
 
-Cu.import("resource://gre/modules/Timer.jsm");
+const { setTimeout } = ChromeUtils.import("resource://gre/modules/Timer.jsm");
 
 keefox_win.UninstallHelper = function()
 {

xul-ext/chrome/content/context.js
@@ -20,8 +20,8 @@
 */
 "use strict";
 
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");
-Cu.import("resource://kfmod/kfDataModel.js");
+// const { XPCOMUtils } = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+// const { keeFoxLoginInfo, keeFoxLoginField, keeFoxFormFieldType } = ChromeUtils.import("resource://kfmod/kfDataModel.js");
 
 keefox_win.context = {
 

xul-ext/chrome/content/famsOptions.js
@@ -20,7 +20,7 @@
 
 let Cu = Components.utils;
 
-Cu.import("resource://kfmod/FAMS.js");
+const { FirefoxAddonMessageService, keeFoxGetFamsInst } = ChromeUtils.import("resource://kfmod/FAMS.js");
 
 var FAMS = null;
 var config = null;

xul-ext/chrome/content/framescript/formsFillTab.js
@@ -23,7 +23,7 @@
 */
 "use strict";
 
-Cu.import("resource://kfmod/kfDataModel.js");
+const { keeFoxLoginInfo } = ChromeUtils.import("resource://kfmod/kfDataModel.js");
 
 var _calculateFieldMatchScore = function (formField, dataField, currentPage, overWriteFieldsAutomatically)
 {

xul-ext/chrome/content/framescript/formsSaveTab.js
@@ -25,7 +25,7 @@
 */
 "use strict";
 
-Cu.import("resource://kfmod/kfDataModel.js");
+const { keeFoxLoginInfo } = ChromeUtils.import("resource://kfmod/kfDataModel.js");
 
 var formSubmitObserver = {
     notify: function (form, domWin, actionURI, cancelSubmit) {

commit 14da90d22117f9a72fe2ae93c88597e1ef05ba8d

fix undefined window.keeFoxLoginInfo
This was probably broken by 7b3959d1af8883d88d04dbdac5c7ac319ce7ca18.

We don't need to add it to the window global in TB anyway, so just import it directly.

xul-ext/chrome/content/KFcommonDialog.js
@@ -38,6 +38,7 @@ if (!Cu)
     var Cu = Components.utils;
 
 const { keefox_org } = ChromeUtils.import("resource://kfmod/KF.js");
+const { keeFoxLoginInfo } = ChromeUtils.import("resource://kfmod/kfDataModel.js");
 
 var keeFoxDialogManager = {
     scriptLoader : Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
@@ -660,7 +661,7 @@ var keeFoxDialogManager = {
             
             for (var i in logins)
             {
-                var kfl = window.keeFoxLoginInfo();
+                var kfl = keeFoxLoginInfo();
                 kfl.initFromEntry(logins[i]);
                 convertedResult.push(kfl);
             }

xul-ext/chrome/content/gdata-provider.js
@@ -34,6 +34,7 @@ if (!Cu)
     var Cu = Components.utils;
 
 const { keefox_org } = ChromeUtils.import("resource://kfmod/KF.js");
+const { keeFoxLoginInfo } = ChromeUtils.import("resource://kfmod/kfDataModel.js");
 
 var keeFoxGDataProviderHelper = {
     scriptLoader : Components.classes["@mozilla.org/moz/jssubscript-loader;1"]
@@ -246,7 +247,7 @@ var keeFoxGDataProviderHelper = {
 
             for (var i in logins)
             {
-                var kfl = window.keeFoxLoginInfo();
+                var kfl = keeFoxLoginInfo();
                 kfl.initFromEntry(logins[i]);
                 convertedResult.push(kfl);
             }