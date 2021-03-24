b05d7b74b6e35ecadd3fb72448a9e363ddfce43f
antixn <xavier.nogue@gmail.com>
2021-03-24 21:21:30 +0100

    WindowListener-API

---
+++ xul-ext/background.js
@@ -0,0 +1,46 @@
+/**
+ * commented out locales are not complete enough on Transifex
+ */
+const locales = [
+    'cs', 
+    //'da', 
+    'de', 
+    //'el', 
+    'en-US', 
+    'es-AR', 
+    'fi', 
+    'fr', 
+    //'hi',
+    'hu',
+    //'it', 
+    //'ja', 
+    //'ko-KR', 
+    'nl', 
+    //'pl', 
+    //'pt-BR', 
+    //'pt-PT', 
+    //'ro', 
+    'ru', 
+    'sl', 
+    //'sv-SE', 
+    'tr', 
+    'ul', 
+    'zh-CN'
+];
+(async () => {
+    // https://github.com/thundernest/addon-developer-support/wiki/WindowListener-API:-Getting-Started#windowlistenerregisterdefaultprefs
+    messenger.WindowListener.registerDefaultPrefs("defaults/preferences/prefs.js")
+    /**
+     * https://github.com/thundernest/addon-developer-support/wiki/WindowListener-API:-Getting-Started#windowlistenerregisterchromeurl
+     * Register the content, resource and locale entries from your legacy chrome.manifest via a call to registerChromeUrl().
+     */
+    messenger.WindowListener.registerChromeUrl([ 
+        ["content",  "keefox",                  "chrome/content/"],
+        ["resource", "keefox", "classic/1.0",   "chrome/skin/"],
+        ["resource", "kfmod",                   "modules/"],
+        ...locales.map((localeId) => 
+        ["locale",   "keefox", localeId,        `chrome/locale/${localeId}/`])
+    ]);
+    // https://github.com/thundernest/addon-developer-support/wiki/WindowListener-API:-Getting-Started#windowlistenerregisteroptionspage
+    messenger.WindowListener.registerOptionsPage("chrome://keefox/content/options.xul")
+})()
